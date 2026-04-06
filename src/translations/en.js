15fc840487a08f275f2769bfbb903edf8155b932
EthanKrako <77197427+EthanKrako@users.noreply.github.com>
2026-04-06 23:55:16 +0200

    projects description and styling

--- src/translations/en.js
+++ src/translations/en.js
@@ -8,8 +8,24 @@ export default {
     "project-pro": "Pro",
     "project-perso": "Perso",
     "website-link": "See website ↪",
+    "github-link": "GitHub link ↪",
     
     // Projects
-    "pro-title-1": "Project Title",
-    "pro-desc-1": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "pro-title-1": "Odyssea",
+    "pro-desc-1": "Participated in the creation of a new website for Odyssea, focusing mainly on frontend integration. I worked mainly on the homepage, landing pages, and blog. The main challenges in this project were optimizing site performance, ensuring full responsiveness across devices, and properly integrating the cookie system, especially for video content.",
+
+    "pro-title-2": "Seine-Saint-Denis Habitat",
+    "pro-desc-2": "Development of a script for importing residences, as well as creating an interactive map and a dedicated listing page. The project involved setting up a geocoding system, using city APIs to display services on the map, and implementing filters for the map and the residence listing.",
+
+    "pro-title-3": "Sporsora",
+    "pro-desc-3": "The goal of this project was to migrate the existing Sporsora website to WordPress. I worked both on the frontend and backend to recreate a 1 to 1 copy of the previous site while improving performance and maintainability.",
+
+    "perso-title-1": "Job Application Tracker",
+    "perso-desc-1": "A React/Express application for tracking job applications, to organize and keep track of CVs sent to different companies. The primary goal of this project was to play a bit with JWT tokens and do a more in-depth exploration of the inner workings of React, especially how the rendering and commit phases work.",
+
+    "perso-title-2": "Collaborative Text Editor",
+    "perso-desc-2": "A real-time collaborative text editor built with Angular and NestJS. The goal of this project was purely to play with WebSockets and real-time synchronization, using Angular's RxJS, Socket.IO and Yjs to achieve seamless collaboration, while still being able to save and load documents from the database.",
+
+    "perso-title-3": "This Portfolio",
+    "perso-desc-3": "The portfolio website you are currently browsing. This is a simple site built with vanilla JavaScript, HTML and CSS, to showcase my projects and skills.",
 }
--- src/translations/fr.js
+++ src/translations/fr.js
@@ -8,8 +8,24 @@ export default {
     "project-pro": "Pro",
     "project-perso": "Perso",
     "website-link": "Voir le site ↪",
+    "github-link": "Lien GitHub ↪",
 
     // Projets
-    "pro-title-1": "Project Title",
-    "pro-desc-1": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "pro-title-1": "Odyssea",
+    "pro-desc-1": "Création d'un nouveau site web pour Odyssea, en se concentrant principalement sur l'intégration frontend. J'ai travaillé principalement sur la page d'accueil, les landing pages et les actualités. Les principaux challenges de ce projet reposaient sur l'optimisation des performances du site, l'assurance d'une parfaite réactivité sur tous les appareils, et l'intégration appropriée du système de cookies, en particulier pour le contenu vidéo.",
+
+    "pro-title-2": "Seine-Saint-Denis Habitat",
+    "pro-desc-2": "Développement d’un script permettant l’import de résidences, ainsi que la création d’une carte interactive et d’une page de listing dédiée. Le projet a impliqué la mise en place d’un système de géocodage, l’utilisation des API de la ville pour afficher les services sur la carte, ainsi que l’implémentation de filtres pour la carte et le listing des résidences.",
+
+    "pro-title-3": "Sporsora",
+    "pro-desc-3": "Le but de ce projet était de migrer le site existant de Sporsora vers WordPress. J'ai travaillé à la fois sur le frontend et le backend, pour recréer une copie conforme du site précédent tout en améliorant les performances et la maintenabilité.",
+
+    "perso-title-1": "Job Application Tracker",
+    "perso-desc-1": "Une application React/Express de suivi de candidatures d'emploi, pour organiser et suivre les CV envoyés à différentes entreprises. L'objectif premier de ce projet était de pouvoir jouer peu avec les tokens JWT et d'explorer plus en profondeur le fonctionnement interne de React, notamment le fonctionnement des phases de rendu et de commit.",
+
+    "perso-title-2": "Collaborative Text Editor",
+    "perso-desc-2": "Un éditeur de texte collaboratif en temps réel construit avec Angular et NestJS. Le but de ce projet était simplement de jouer avec les WebSockets et la synchronisation en temps réel, en utilisant RxJS, Socket.IO et Yjs pour permettre une collaboration fluide, tout en étant capable de sauvegarder et charger les documents depuis la base de données.",
+
+    "perso-title-3": "Portfolio",
+    "perso-desc-3": "Le porfolio que vous consultez actuellement. J'ai voulu créer un site simple en JavaScript, HTML et CSS vanilla, pour mettre en avant mes différents projets.",
 }
