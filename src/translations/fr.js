c468508054b6701460e51402b02a735f968ce0c0
EthanKrako <77197427+EthanKrako@users.noreply.github.com>
2026-04-11 16:29:34 +0200

    translation adjustment

--- src/translations/fr.js
+++ src/translations/fr.js
@@ -1,7 +1,7 @@
 export default {
     "lang-abv": "FR",
     "hero-title": "Hey ! Je suis <strong>Ethan</strong>,",
-    "hero-subtitle": "Un développeur Full Stack principalement expérimenté en développement web, mais aussi très intéressé par l'architecture informatique et les sujets bas niveau.",
+    "hero-subtitle": "Un développeur Full Stack principalement expérimenté en développement web, mais aussi très intéressé par l'architecture des systèmes informatiques et la programmation bas niveau.",
     "hero-subtitle-2": "J'aime comprendre comment les choses fonctionnent à tous les niveaux, et appliquer ces connaissances pour un code maintenable, efficace et sûr.",
     "profile picture": "Photo de profil",
     "project-title": "Projets",
